ec164437f3154600284f3c8c08aa4c65ab7f0400
wjdrud2532 <wjdrud2532@naver.com>
2024-07-04 16:20:33 +0900

    for LLVM JIT
    Detect Architectures and JIT Option (Line : 58~70)
    
    add Cross Compiler for AARCH64 (LINE : 83~91

--- samples/littlevgl/build.sh
+++ samples/littlevgl/build.sh
@@ -44,11 +44,63 @@ if [ ! -d "lvgl" ]; then
 fi
 
 echo "##################### 0. build wamr-sdk littlevgl start#####################"
-
 cd ${WAMR_APP_FRAMEWORK_DIR}/wamr-sdk
 ./build_sdk.sh -n littlevgl -x ${PROJECT_DIR}/wamr_config_littlevgl.cmake -e ${LV_CFG_PATH} -c
 [ $? -eq 0 ] || exit $?
 echo "#####################build wamr-sdk littlevgl success"
+
+
+
+# wamr_config_littlevgl.cmake 파일 경로 설정
+config_file="${PWD}/../wamr_config_littlevgl.cmake"
+
+# wamr_config_littlevgl.cmake 파일에서 WAMR_BUILD_TARGET 값을 추출합니다.
+target_value=$(grep 'WAMR_BUILD_TARGET' "$config_file" | sed -E 's/.*\s+([^\)]+)\).*/\1/')
+jit_value=$(grep 'WAMR_BUILD_JIT' "$config_file" | sed -E 's/.*\s+([^\)]+)\).*/\1/')
+
+if [ "$jit_value" -eq 1 ]; then
+
+    echo "##################### 0.5. build LLVM start#####################"
+    echo "JIT is enbled, need llvm build!"
+    echo " WAMR_BUILD_TARGET:$target_value";
+
+    # rm -rf ${WAMR_DIR}/core/deps/llvm/build
+
+    #target 확인 
+    if [ "$target_value" = "X86_64" ]; then
+
+        echo "##################### X86_64 target is selected, LLVM build start #####################"
+        cp ${PROJECT_DIR}/llvm_build_script_aarch64/build_llvm_x86_64.py ${WAMR_DIR}/build-scripts/build_llvm_x86_64.py
+        cp ${PROJECT_DIR}/llvm_build_script_aarch64/build_llvm_x86_64.sh ${WAMR_DIR}/build-scripts/build_llvm_x86_64.sh
+        cd ${WAMR_DIR}/wamr-compiler
+        ./build_llvm_x86_64.sh
+    
+    elif [ "$target_value" = "AARCH64" ]; then
+
+        echo " ##################### AARCH64 target is selected, LLVM build start #####################"
+        file_path="/home/gcc-linaro-14.0.0-2023.06-x86_64_aarch64-linux-gnu.tar.xz"
+    
+        if [ -f "$file_path" ]; then
+            echo " ##################### linaro-14 exists"
+        else
+            echo " ##################### linaro-14 deos not exists, download linaro-14"
+            cd /home
+            wget https://snapshots.linaro.org/gnu-toolchain/14.0-2023.06-1/aarch64-linux-gnu/gcc-linaro-14.0.0-2023.06-x86_64_aarch64-linux-gnu.tar.xz
+            tar -xvf gcc-linaro-14.0.0-2023.06-x86_64_aarch64-linux-gnu.tar.xz -C /home
+            echo " ##################### linaro-14 download complete"
+        fi
+
+        cp ${PROJECT_DIR}/llvm_build_script_aarch64/build_llvm_aarch64.py ${WAMR_DIR}/build-scripts/build_llvm_aarch64.py
+        cp ${PROJECT_DIR}/llvm_build_script_aarch64/build_llvm_aarch64.sh ${WAMR_DIR}/build-scripts/build_llvm_aarch64.sh
+        cd ${WAMR_DIR}/wamr-compiler
+        ./build_llvm_aarch64.sh
+
+    else
+        echo "WIP, not supported target"
+    fi
+
+    echo ""##################### LLVM build complete "#####################"
+fi
 
 echo -e "\n\n"
 echo "##################### 1. build native-ui-app start#####################"
